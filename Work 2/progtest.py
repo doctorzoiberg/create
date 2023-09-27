42b018836443a430fa96dcc2227b54c216f13517
doctorzoiberg <129968489+doctorzoiberg@users.noreply.github.com>
2023-09-27 20:26:03 +0300

    Update progtest.py
    Изменил на пол секунды

--- Work 2/progtest.py
+++ Work 2/progtest.py
@@ -5,7 +5,7 @@ def generate_random_numbers():
     for _ in range(20):
         random_number = random.randint(35000, 98000)
         print(random_number)
-        time.sleep(0.2)
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     generate_random_numbers()
