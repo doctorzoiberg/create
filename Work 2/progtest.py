b79ce3d90fac35226e0c373abec5ff7c5a7cc1c5
ArinaMartynova <144528661+ArinaMartynova@users.noreply.github.com>
2023-09-27 14:35:49 +0300

    Add files via upload

---
+++ Work 2/progtest.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+def generate_random_numbers():
+    for _ in range(20):
+        random_number = random.randint(35000, 98000)
+        print(random_number)
+        time.sleep(0.2)
+
+if __name__ == "__main__":
+    generate_random_numbers()
